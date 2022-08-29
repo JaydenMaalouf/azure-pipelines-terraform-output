aea859f5125930534e02b6001ab104806d55ac5f
JaydenMaalouf <jayden.m.maalouf@gmail.com>
2022-08-29 11:20:05 +1000

    first commit

---
+++ tasks/TerraformOutput/src/index.ts
@@ -0,0 +1,43 @@
+import path = require("path");
+import task = require("azure-pipelines-task-lib/task");
+import { TaskResult } from "azure-pipelines-task-lib/task";
+import { IExecOptions } from "azure-pipelines-task-lib/toolrunner";
+
+async function run() {
+  let terraformPath: string;
+  try {
+    terraformPath = task.which("terraform", true);
+  } catch (err) {
+    throw "Terraform CLI not found.";
+  }
+
+  const terraformTool = task.tool(terraformPath);
+  const outputFilePath = task.getPathInput("outputFilePath");
+  const workingDirectory = path.dirname(outputFilePath);
+  terraformTool.arg(["show", outputFilePath]);
+
+  const result = terraformTool.execSync(<IExecOptions>{
+    cwd: workingDirectory,
+    silent: true
+  });
+
+  if (result.code != 0) {
+    console.log(JSON.stringify(result));
+    throw `Terraform Output failed with Exit Code: ${result.code}
+    Error Message: ${result.error.message}`;
+  }
+
+  task.debug(`Output file fetched: ${result.stdout}`);
+  const artifactName = task.getInput("artifactName");
+  const outputFile = path.join(task.getVariable("Build.StagingDirectory"), artifactName);
+  task.writeFile(outputFile, result.stdout);
+  task.debug(`Output file written: ${outputFile}`);
+  task.addAttachment("terraform.plan", artifactName, outputFile);
+  console.log(`Uploaded Plan Output.`);
+}
+
+run().then(() => {
+  task.setResult(TaskResult.Succeeded, "");
+}).catch((error) =>  {
+  task.setResult(TaskResult.Failed, error);
+});
