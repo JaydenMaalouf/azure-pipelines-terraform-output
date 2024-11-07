e4bd3c32a1ca294c48f40e27b26d365c3f378a5d
Jayden Maalouf <jayden.m.maalouf@gmail.com>
2024-11-07 17:02:19 +1100

    fix: :bug: node version warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,48 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const webpack = require('webpack')
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    TerraformTab: './src/TerraformTab/tabContent.tsx',
+    TerraformTab: './src/TerraformTab/tabContent.tsx'
   },
   output: {
-    filename: '[name]/tabContent.js',
+    filename: '[name]/tabContent.js'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      'azure-devops-extension-sdk': path.resolve(
-        'node_modules/azure-devops-extension-sdk',
-      ),
+      'azure-devops-extension-sdk': path.resolve('node_modules/azure-devops-extension-sdk')
     },
     fallback: {
-      buffer: require.resolve('buffer'),
-    },
+      buffer: require.resolve('buffer')
+    }
   },
   stats: {
-    warnings: false,
+    warnings: false
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: 'ts-loader'
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'azure-devops-ui/buildScripts/css-variables-loader',
-          'sass-loader',
-        ],
+        use: ['style-loader', 'css-loader', 'azure-devops-ui/buildScripts/css-variables-loader', 'sass-loader']
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   plugins: [
-    new CopyWebpackPlugin([
-      { from: '**/*.html', context: 'src/' },
-      { from: '**/*.css', context: 'src/' },
-    ]),
-    new webpack.ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
+    new CopyWebpackPlugin({
+      patterns: [{ from: '**/*.html', context: 'src/' }]
     }),
-  ],
-}
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]
+};
